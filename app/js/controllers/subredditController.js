a606745df2227a3c5fc233b9050ec1c4fa245f07
Nicholas Murray <murray.nicholas@gmail.com>
2014-10-09 00:25:29 +0100

    Added selecting of subreddit and passing to controller with a default subreddit of bicycling

--- app/js/controllers/subredditController.js
+++ app/js/controllers/subredditController.js
@@ -1,7 +1,20 @@
 app.controller('subredditCtrl', function($scope, subredditService) {
 
-	subredditService.getSubredditPostsTitles("bicycling").then(function(postsTitles) {
-	  $scope.postsTitles = postsTitles;
-	});
+	var defaultSubreddit = "bicycling";
+	var selectedSubreddit = defaultSubreddit;
+	var callSubredditService = function() {
+		subredditService.getSubredditPostsTitles(selectedSubreddit).then(function(postsTitles) {
+			$scope.postsTitles = postsTitles;
+		});
+	}
+
+	// onchange
+	$scope.changed = function() {
+			selectedSubreddit = $scope.subreddits
+			callSubredditService();
+	}
+
+	// default
+	callSubredditService();
 
 })
